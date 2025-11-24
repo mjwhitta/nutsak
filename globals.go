f153c4f9f1c55e1154131b9542b62dc9ffabe282
Miles Whittaker <mj@whitta.dev>
2025-11-24 15:24:49 -0500

    Updated deps

--- globals.go
+++ globals.go
@@ -3,7 +3,7 @@ package nutsak
 import "github.com/mjwhitta/log"
 
 // Version is the package version.
-const Version string = "1.1.5"
+const Version string = "1.1.6"
 
 //nolint:grouper // This is an iota block
 const (
